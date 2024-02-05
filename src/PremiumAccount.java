925ae67110b658e09327597e4597712b959b9258
nicowiener812 <39963972+nicowiener812@users.noreply.github.com>
2024-02-05 00:05:04 -0300

    Initial commit

--- src/PremiumAccount.java
+++ src/PremiumAccount.java
@@ -1,1 +1,42 @@
 public class PremiumAccount implements IAccount {
+    private int accountNumber;
+    private double balance;
+
+    public PremiumAccount(int accountNumber) {
+        this.accountNumber = accountNumber;
+        this.balance = 0;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    @Override
+    public double withdraw(double amount) {
+        if (amount < 0) {
+            return 0;
+        }
+        if (amount > balance) {
+            double maxWithdrawal = balance;
+            balance = 0;
+            return maxWithdrawal;
+        } else {
+            balance -= amount;
+            return amount;
+        }
+    }
+
+    @Override
+    public double getCurrentBalance() {
+        return balance;
+    }
+
+    @Override
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+}
+
